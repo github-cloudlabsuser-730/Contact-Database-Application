4946b0d10295591a7475be56b791a4d4737b6a24
github_cloudlabsuser_730@cloudlabsaiuser.com <github_cloudlabsuser_730@cloudlabsaiuser.com>
2024-05-04 23:51:29 +0000

    The most significant changes include the modification of the `UserController.cs` file to implement the `Details`, `Create`, `Edit`, and `Delete` methods and to update the `Index` method to accept a `searchString` parameter for user filtering. The `Index.cshtml` file was also updated to include a form for searching users. Additionally, the `CRUD application 2.sln` file was updated to change the Visual Studio version and add a new project.
    1. The `CRUD application 2.csproj` file was modified to include and later remove a reference to `Microsoft.CodeDom.Providers.DotNetCompilerPlatform` with version `2.0.1.0`. This change does not affect the functionality of the code.
    2. The `CRUD application 2.csproj` file was also modified to remove and then add back the closing `Project` tag. This change seems to be unnecessary as it doesn't affect the functionality of the code.
    3. The `CRUD application 2.sln` file was updated to change the Visual Studio version from `16.0.33801.447` to `17.9.34622.214`. A new project `Usercontrollertest` was added to the solution.
    4. The `UserController.cs` file was significantly updated. The `Index` method now accepts a `searchString` parameter and filters the users based on this string. The `Details`, `Create`, `Edit`, and `Delete` methods were implemented. The `Edit` method no longer updates the user's age.
    5. The `Index.cshtml` file was updated to include a form for searching users. The display of users in a table was slightly modified.
    
    The most significant changes include the extensive modifications to the `UserController.cs` file, which now includes a `searchString` parameter to the `Index` method and the implementation of the `Details`, `Create`, `Edit`, and `Delete` methods. Additionally, the `Index.cshtml` file has been updated to include a search form and display a list of users in a table. The Visual Studio version used for the project `CRUD application 2.sln` has also been updated.
    
    Here is a list of the changes:
    
    1. The project file `CRUD application 2.csproj` was modified to include a reference to `Microsoft.CodeDom.Providers.DotNetCompilerPlatform` version `2.0.1.0`, which was later removed in another change. This change might have been made to test the compatibility or functionality of the project with this particular version of the compiler platform.
    
    2. The Visual Studio version used for the project `CRUD application 2.sln` was updated from version `16.0.33801.447` to version `17.9.34622.214`. This update might have been made to take advantage of new features or improvements in the newer version of Visual Studio.
    
    3. A new project `Usercontrollertest` was added to the solution `CRUD application 2.sln`. This new project might have been created to test the functionality of the user controller in isolation from the rest of the application.
    
    4. The `UserController.cs` file was extensively modified. The `searchString` parameter was added to the `Index` method, and the `Details`, `Create`, `Edit`, and `Delete` methods were implemented. Comments were also added to explain the purpose of each method. These changes were likely made to enhance the functionality of the user controller and improve code readability.
    
    5. The `Index.cshtml` file was modified to include a search form and display the list of users in a table. These changes were likely made to improve the user interface and user experience of the application.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using CRUD_application_2.Models;
+using System;
 using System.Linq;
 using System.Web.Mvc;
  
@@ -8,59 +9,116 @@ namespace CRUD_application_2.Controllers
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
         // GET: User
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
-            // Implement the Index method here
+            var users = userlist;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Name.Contains(searchString) || s.Email.Contains(searchString)).ToList();
+            }
+
+            return View(users);
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
- 
+
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                // Add the new user to the userlist
+                userlist.Add(user);
+                // Redirect to the Index action to display the updated list of users
+                return RedirectToAction("Index");
+            }
+            // If there are validation errors, return the Create view to display the errors
+            return View(user);
         }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Update the existing user's information
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                //existingUser.Age = user.Age;
+
+                // Redirect to the Index action to display the updated list of users
+                return RedirectToAction("Index");
+            }
+
+            // If there are validation errors, return the Edit view to display the errors
+            return View(user);
         }
- 
+
         // GET: User/Delete/5
+        // This action method handles the HTTP GET request to delete a user.
+        // It receives the user's ID as a parameter.
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            // Find the user in the userlist using the provided ID.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+
+            // If the user is not found in the userlist, return a 404 Not Found status.
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            // If the user is found, return the Delete view for that user.
+            return View(user);
         }
- 
+
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
+               
     }
 }
